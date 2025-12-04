3246a1ee8833ddca50ded127e4f693c65042c9f2
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-12-04 12:50:11 -0300

    .

--- src/DadosEstoque/Aplication/Program.java
+++ src/DadosEstoque/Aplication/Program.java
@@ -22,7 +22,24 @@ public class Program {
         product.quantity=sc.nextInt();
 
         System.out.println();
-        System.out.print("Product data: " +product.name + ", " + product.price + ", " + product.quantity);
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be remove from stock: ");
+         quantity = sc.nextInt();
+         product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+
 
 
         sc.close();
