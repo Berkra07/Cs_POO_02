5af9787936ccf725697e484e87e6cbdc2b1a6416
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2025-12-10 20:31:16 -0300

    Create Employee.java

---
+++ src/AumentoSalarioPorcentagem/entities/Employee.java
@@ -0,0 +1,20 @@
+package AumentoSalarioPorcentagem.entities;
+
+public class Employee {
+
+    //Atributos da classe
+
+    public String name;
+    public double grossSalary;
+    public double tax;
+
+    //Métodos da classe
+
+    public double NetSalary(){
+        return grossSalary - tax;
+    }
+
+    public void IncreaseSalary(double percentage){
+        this.grossSalary += this.grossSalary * percentage / 100;
+    }
+}
