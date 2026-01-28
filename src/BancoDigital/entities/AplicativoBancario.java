d701f7c6186c99be5ea25eb10dbf2a400af07293
bernardo kravtschke <164900207+Berkra07@users.noreply.github.com>
2026-01-27 22:45:13 -0300

    Add AplicativoBancario entity class
    Introduces the AplicativoBancario class with account number, account holder name, and balance fields. Includes methods for setting initial balance, depositing, and withdrawing funds.

---
+++ src/BancoDigital/entities/AplicativoBancario.java
@@ -0,0 +1,49 @@
+package BancoDigital.entities;
+
+public class AplicativoBancario {
+
+    private int numeroConta;
+    private String nomeTitular;
+    private double saldoConta;
+
+    public AplicativoBancario(int numeroConta, String nomeTitular) {
+        this.numeroConta = numeroConta;
+        this.nomeTitular = nomeTitular;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    public double getSaldoConta() {
+        return saldoConta;
+    }
+
+
+    public void saldoInicial (double valor){
+        if (valor > 0) {
+           this.saldoConta = valor;
+        }
+    }
+
+    public void depositar (double valor){
+        if (valor > 0){
+        this.saldoConta += valor;
+        }
+    }
+
+    public void Sacar(double valor){
+        if (valor > 0 && valor <= saldoConta){
+        this.saldoConta -= valor;
+        }
+    }
+
+}
