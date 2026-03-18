58c0095c2dd996310f0792883e06974652f58c69
Bernardo kravutschke <164900207+Berkra07@users.noreply.github.com>
2026-03-18 16:04:38 -0300

    Update Calculator.java

--- src/RectangleCalculator/aplication/Calculator.java
+++ src/RectangleCalculator/aplication/Calculator.java
@@ -6,7 +6,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args) {
+     static void main(String[] ignore) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
