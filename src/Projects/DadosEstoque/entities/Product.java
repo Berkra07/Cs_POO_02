6767e3329d160dc58b8dc60a5911253c31e6ecf6
Bernardo kravutschke <164900207+Berkra07@users.noreply.github.com>
2026-04-01 12:51:30 -0300

    Organize packages and add new project programs

---
+++ src/Projects/DadosEstoque/entities/Product.java
@@ -0,0 +1,64 @@
+package Projects.DadosEstoque.entities;
+
+public class Product {
+
+    //Atributos da classe
+
+    private String name;
+    private double price;
+    private int quantity;
+
+
+   //Construtores
+
+    public Product (String name, double price){
+        this.name = name;
+        this.price = price;
+        quantity = 1;
+    }
+    //Getters e setters
+
+    public String getName( ){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;    }
+
+
+
+    //Metodos
+    public double totalValueInStock(){
+        return price * quantity;
+    }
+
+    public void addProducts(int quantity){
+
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity){
+
+        this.quantity -= quantity;
+    }
+
+    public String toString(){
+        return name
+                + ", $ "
+                + String.format("%.2f",price)
+                +", "
+                +quantity
+                + " units, Total: $ "
+                +totalValueInStock();
+    }
+
+
+}
