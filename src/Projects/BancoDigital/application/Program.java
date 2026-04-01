48d812fc0e8d1147c9e9a194f91898e8f2e65404
Bernardo kravutschke <164900207+Berkra07@users.noreply.github.com>
2026-04-01 12:51:21 -0300

    Organize packages and add new project programs

---
+++ src/Projects/BancoDigital/application/Program.java
@@ -0,0 +1,71 @@
+package Projects.BancoDigital.application;
+
+import Projects.BancoDigital.entities.AplicativoBancario;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        double valor ;
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account number: "); //Numero da conta
+        int numeroConta = sc.nextInt();
+
+        System.out.println("Enter account holder: ");//Name
+        String nomeTitular = sc.next();
+
+        AplicativoBancario appBK = new AplicativoBancario(numeroConta,nomeTitular);
+
+
+        System.out.println("Is there na initial deposit (y/n)? ");
+        sc.nextLine(); // limpar buffer
+        String resposta = sc.nextLine();
+
+        if (resposta.equals("y")) {
+            System.out.println("Enter  initial deposit value: ");
+            valor = sc.nextInt();
+            appBK.saldoInicial(valor);
+            System.out.printf(
+                    "Account data:\n Account %d, Holder: %s, Balance: $ %.2f%n",
+                    numeroConta,
+                    nomeTitular,
+                    appBK.getSaldoConta()
+            );
+        }
+        else {        System.out.printf(
+                "Account data:\n Account %d, Holder: %s, Balance: $ %.2f%n",
+                numeroConta,
+                nomeTitular,
+                appBK.getSaldoConta()
+        );
+        }
+
+        System.out.println();//pular linha
+
+        System.out.println("Ente a deposit value: ");
+        valor = sc.nextInt();
+        appBK.depositar(valor);
+        System.out.printf(
+                "Account data:\n Account %d, Holder: %s, Balance: $ %.2f%n",
+                numeroConta,
+                nomeTitular,
+                appBK.getSaldoConta()
+        );
+
+        System.out.println();//pular linha
+
+        System.out.println("Enter a withdraw value: ");
+        valor=sc.nextInt();
+        appBK.Sacar(valor);
+        System.out.printf(
+                "Account data:\n Account %d, Holder: %s, Balance: $ %.2f%n",
+                numeroConta,
+                nomeTitular,
+                appBK.getSaldoConta()
+        );
+
+    }
+}
---
+++ src/Projects/CalculoAreaTriangulo/application/Program.java
@@ -0,0 +1,41 @@
+package Projects.CalculoAreaTriangulo.application;
+
+import Projects.CalculoAreaTriangulo.entities.Triangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc= new Scanner(System.in);
+
+        Triangle x,y;
+        x=new Triangle();
+        y=new Triangle();
+
+        System.out.println(" Enter the measures of triangle X:");
+        x.a= sc.nextDouble();
+        x.b= sc.nextDouble();
+        x.c= sc.nextDouble();
+        System.out.println(" Enter the measures of triangle Y:");
+        y.a= sc.nextDouble();
+        y.b= sc.nextDouble();
+        y.c= sc.nextDouble();
+
+        double areaX=x.area(); //Raiz quadrada para descobrir a area X
+        double areaY= y.area(); //Raiz quadrada para descobrir a area Y
+
+        System.out.printf("triangle X area: %.4f%n", areaX);
+        System.out.printf("triagle Y area: %.4f%n", areaY);
+
+        if (areaX > areaY){
+            System.out.println("Larger area: Y");
+        }
+        else {
+            System.out.println("Large area: Y");
+        }
+        sc.close();
+    }
+}
