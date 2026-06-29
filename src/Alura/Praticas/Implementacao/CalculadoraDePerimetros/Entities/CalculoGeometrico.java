7b68fe00d3eba52b94ae40b3b19304011a2e675e
Win <bernardokravutschkegomes@gmail.com>
2026-06-29 20:34:29 -0300

    Studying

---
+++ src/Alura/Praticas/Implementacao/CalculadoraDePerimetros/Entities/CalculoGeometrico.java
@@ -0,0 +1,8 @@
+package Alura.Praticas.Implementacao.CalculadoraDePerimetros.Entities;
+
+public interface CalculoGeometrico {
+
+    void calcularArea(double altura, double largura);
+    void calcularPerimetro(double altura, double largura);
+
+}
