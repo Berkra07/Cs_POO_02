638c7c9da08a3c00767b081dda211f304584e1c7
Win <bernardokravutschkegomes@gmail.com>
2026-06-29 20:39:59 -0300

    Studying

--- src/Alura/Praticas/Implementacao/CalculadoraDePerimetros/Application/AppCalculadorDePerimetros.java
+++ src/Alura/Praticas/Implementacao/CalculadoraDePerimetros/Application/AppCalculadorDePerimetros.java
@@ -10,15 +10,21 @@ public class AppCalculadorDePerimetros extends CalculadoraSalaRetangular {
         double altura,largura;
 
         CalculadoraSalaRetangular sala1 = new CalculadoraSalaRetangular();
+        System.out.print("Olá. digite seu nome: ");
+        String nome = sc.next();
 
         System.out.println("Digite a metragem de altura da sala: ");
         altura = sc.nextDouble();
         System.out.println("Digite a metragem da largura da sala: ");
         largura = sc.nextDouble();
+        System.out.println("  Olá " + nome + ", aqui estão os dados da sua casa: \n " +
+                "--------------------------------------------------------------------------------------------------- \n" +
+                " DADOS DA CASA:");
 
         sala1.calcularArea(altura,largura);
         sala1.calcularPerimetro(altura,largura);
 
+        System.out.println("--------------------------------------------------------------------------------------------------- \nb");
 
         sc.close();
     }
