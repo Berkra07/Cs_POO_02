363536dc1f432252347a10cdbe11c2a9b86e56ac
Win <bernardokravutschkegomes@gmail.com>
2026-06-29 20:34:35 -0300

    Studying

---
+++ src/Alura/Praticas/Implementacao/CalculadoraDePerimetros/Entities/CalculadoraSalaRetangular.java
@@ -0,0 +1,16 @@
+package Alura.Praticas.Implementacao.CalculadoraDePerimetros.Entities;
+
+public class CalculadoraSalaRetangular implements CalculoGeometrico {
+    @Override
+    public void calcularArea(double altura, double largura) {
+        double calculoArea  = altura *  largura ;
+        System.out.println("A area da sua sala é : " + calculoArea + "m²");
+    }
+
+    @Override
+    public void calcularPerimetro(double altura, double largura) {
+        double caluloPerimetro = altura * 2 + largura * 2;
+        System.out.println("O perimetro da sua sala é :" + caluloPerimetro + " metros");
+
+    }
+}
