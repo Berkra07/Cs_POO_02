00545ec9d5f56048981dc470a7c4e62cc3e8a32f
Win <bernardokravutschkegomes@gmail.com>
2026-07-01 19:50:47 -0300

    Studying

---
+++ src/Alura/Praticas/Implementacao/ConversorDeTemperatura/Entities/ConversorDeTemperatura.java
@@ -0,0 +1,7 @@
+package Alura.Praticas.Implementacao.ConversorDeTemperatura.Entities;
+
+public interface ConversorDeTemperatura {
+    void celsiusParaFahrenheit(int Celsius );
+    void fahrenheitParaCelsius(int Fahrenheit);
+
+}
