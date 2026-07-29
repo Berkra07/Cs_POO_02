5c20567e6a5f6961cccbe38a2c3da324f21c202a
Win <bernardokravutschkegomes@gmail.com>
2026-07-29 18:51:27 -0300

    Studying

--- src/Alura/Desafios/MinhasMusicas/Entities/Favoritos.java
+++ src/Alura/Desafios/MinhasMusicas/Entities/Favoritos.java
@@ -4,10 +4,14 @@ public class Favoritos {
 
 
     public void inclui (Audio audio) {
-        if (audio.getClassificacao() >= 9) {
+        if (audio.getClassificacao() >= 10) {
             System.out.println(audio.getTitulo() + ", é considerado sucesso absoluto e preferido por todos!");
-        } else {
-            System.out.println(audio.getTitulo()  + ", tambem é um dos que está todo mundo ouvindo");
+        }
+        else if(audio.getClassificacao() >=5) {
+            System.out.println(audio.getTitulo()  + ", tambem é um dos que está todo mundo ouvindo!");
+        }
+        else {
+            System.out.println(audio.getTitulo() + ", Tem potencial de se tornar uma grande música no futuro!");
         }
     }
 
