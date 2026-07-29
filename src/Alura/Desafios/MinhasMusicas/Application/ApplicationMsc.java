e55562b46246c0f0af6e9ce67feca16baea285f5
Win <bernardokravutschkegomes@gmail.com>
2026-07-29 18:51:25 -0300

    Studying

--- src/Alura/Desafios/MinhasMusicas/Application/ApplicationMsc.java
+++ src/Alura/Desafios/MinhasMusicas/Application/ApplicationMsc.java
@@ -3,9 +3,55 @@ package Alura.Desafios.MinhasMusicas.Application;
 import Alura.Desafios.MinhasMusicas.Entities.Favoritos;
 import Alura.Desafios.MinhasMusicas.Entities.Musica;
 
+import java.util.Scanner;
+
 
 public class ApplicationMsc {
     static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== SISTEMA DE CADASTRO ===");
+        System.out.println("1 - Cadastrar Livro");
+        System.out.println("2 - Cadastrar Produto Físico");
+        System.out.print("Escolha uma opção: ");
+        int opcao = sc.nextInt();
+
+        sc.nextLine();
+
+
+        switch (opcao){
+            case 1:
+                System.out.println("\n--- Nova Musica ---");
+                Musica Musica1 = new Musica();
+
+                System.out.println("Digite o nome da musica:");
+                Musica1.setTitulo(sc.nextLine());
+
+                System.out.println("Digite o Cantor/Banda da musica:");
+                Musica1.setCantor(sc.nextLine());
+
+                System.out.println("Digite o nome do album :");
+                Musica1.setAlbum(sc.nextLine());
+
+                System.out.println("");
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         Musica minhaMusica = new Musica();
