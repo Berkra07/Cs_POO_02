4aa58bb2efef4617bb0a78d350eae08b6a798dc9
Bernardo kravutschke <164900207+Berkra07@users.noreply.github.com>
2026-08-10 17:27:56 -0300

    feat(E_Book): add E_Book class with final price calculation method

---
+++ src/Alura/Praticas/Implementacao/vendas/Entities/E_Book.java
@@ -0,0 +1,10 @@
+package Alura.Praticas.Implementacao.vendas.Entities;
+
+public class E_Book extends Products {
+
+
+    public double calcularPrecoFinal() {
+        return priceBase * 0.9;
+
+    }
+}
