6adbc39767d0b5d734f960319ed96e2d807a9271
Bernardo kravutschke <164900207+Berkra07@users.noreply.github.com>
2026-08-10 17:24:48 -0300

    Update import path for CalculadoraDeTempo class

---
+++ src/Alura/Praticas/Implementacao/vendas/application/ApkCompraNet.java
@@ -0,0 +1,15 @@
+package Alura.Praticas.Implementacao.vendas.application;
+
+import Alura.Praticas.Implementacao.vendas.Entities.Products;
+
+import java.util.Scanner;
+
+public class ApkCompraNet extends Products {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        Products products = new Products();
+        System.out.println("Escolha seu produto");
+
+    }
+}
