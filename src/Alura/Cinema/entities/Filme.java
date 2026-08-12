4990562d0d88404497dd85adbc60421316cd7363
Win <bernardokravutschkegomes@gmail.com>
2026-08-12 19:20:22 -0300

    feat: Adiciona SetDiretor

--- src/Alura/Cinema/entities/Filme.java
+++ src/Alura/Cinema/entities/Filme.java
@@ -1,16 +1,20 @@
 package Alura.Cinema.entities;
 import Alura.Cinema.entities.Calculos.Classificavel;
 
-public class Filme extends Titulo implements Classificavel {
 
+public class Filme extends Titulo implements Classificavel {
     private String diretor;
 
     public String getDiretor() {
         return diretor;
     }
 
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
     @Override
     public int getClassificacao() {
-        return (int)pegaMedia() / 2;
+        return (int) pegaMedia() / 2;
     }
 }
