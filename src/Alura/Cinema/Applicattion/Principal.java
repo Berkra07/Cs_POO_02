76c4ecfd9b58566748a8d18cc1ae9ed9d89c68ab
Win <bernardokravutschkegomes@gmail.com>
2026-08-12 19:23:24 -0300

    feat: adiciona exibe ficha tecnica e Filtro de recomendação

--- src/Alura/Cinema/Applicattion/Principal.java
+++ src/Alura/Cinema/Applicattion/Principal.java
@@ -14,6 +14,12 @@ public class Principal {
         meuFilme.setDuracaoEmMinutos(180);
         System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
 
         //meuFilme.somaDasAvaliacoes = 10;
         //meuFilme.totalDeAvaliacoes = 1;
@@ -39,5 +45,13 @@ public class Principal {
         calculadora.inclui(lost);
         System.out.println(calculadora.getTempoTotal());
 
-     }
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
+    }
 }
