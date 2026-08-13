e19b3175c63d63689d5fcfb19eec6454a0245e5d
Win <bernardokravutschkegomes@gmail.com>
2026-08-13 20:35:36 -0300

    Retira gets nunca utilizados

--- src/Alura/Cinema/entities/Filme.java
+++ src/Alura/Cinema/entities/Filme.java
@@ -3,15 +3,9 @@ import Alura.Cinema.entities.Calculos.Classificavel;
 
 
 public class Filme extends Titulo implements Classificavel {
-    private String diretor;
 
-    public String getDiretor() {
-        return diretor;
-    }
 
-    public void setDiretor(String diretor) {
-        this.diretor = diretor;
-    }
+
 
     @Override
     public int getClassificacao() {
