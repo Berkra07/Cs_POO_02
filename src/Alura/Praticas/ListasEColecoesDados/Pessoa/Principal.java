c43d2868d3dbf56f883efc28db6589654a318ad6
Win <bernardokravutschkegomes@gmail.com>
2026-08-13 20:39:02 -0300

    Cria a classe Main Principal, adiciona 3 objetos (pessoas),  adiciona array lista de pessoas mostrando: tamanho da lista, primeira pessoa da lista e lista completa

---
+++ src/Alura/Praticas/ListasEColecoesDados/Pessoa/Principal.java
@@ -0,0 +1,29 @@
+package Alura.Praticas.ListasEColecoesDados.Pessoa;
+
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Pessoas pessoa1 = new Pessoas();
+        pessoa1.setNome("Leandro");
+        pessoa1.setIdade(16);
+
+        Pessoas pessoa2 = new Pessoas();
+        pessoa2.setNome("Casablanca");
+        pessoa2.setIdade(20);
+
+        Pessoas pessoa3 = new Pessoas();
+        pessoa3.setNome("Juvandro");
+        pessoa3.setIdade(39);
+
+        ArrayList<Pessoas> listaPessoas = new ArrayList<>();
+        listaPessoas.add(pessoa1);
+        listaPessoas.add(pessoa2);
+        listaPessoas.add(pessoa3);
+
+        System.out.println("tamanho da lista:" + listaPessoas.size());
+        System.out.println("Primeira pessoa: " + listaPessoas.getFirst());
+        System.out.println("lista de pessoas" + listaPessoas.toString());
+    }
+}
