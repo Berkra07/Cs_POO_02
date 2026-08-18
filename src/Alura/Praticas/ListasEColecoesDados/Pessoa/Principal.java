a2e2ac69419caa789922dc2b79e6615c02208f09
Notebook <bernardokravutschkegomes@gmail.com>
2026-08-18 13:48:07 -0300

    Chore: altera os valores dos getters utilizados para argumentos dos Construtores

--- src/Alura/Praticas/ListasEColecoesDados/Pessoa/Principal.java
+++ src/Alura/Praticas/ListasEColecoesDados/Pessoa/Principal.java
@@ -5,17 +5,16 @@ import java.util.ArrayList;
 public class Principal {
     public static void main(String[] args) {
 
-        Pessoas pessoa1 = new Pessoas();
-        pessoa1.setNome("Leandro");
-        pessoa1.setIdade(16);
+        Pessoas pessoa1 = new Pessoas("Leandro", 20);
 
-        Pessoas pessoa2 = new Pessoas();
-        pessoa2.setNome("Casablanca");
-        pessoa2.setIdade(20);
 
-        Pessoas pessoa3 = new Pessoas();
-        pessoa3.setNome("Juvandro");
-        pessoa3.setIdade(39);
+        Pessoas pessoa2 = new Pessoas("Casablanca",20 );
+//        pessoa2.setNome("Casablanca");
+//        pessoa2.setIdade(20);
+
+        Pessoas pessoa3 = new Pessoas("Juvandro", 39);
+//        pessoa3.setNome("Juvandro");
+//        pessoa3.setIdade(39);
 
         ArrayList<Pessoas> listaPessoas = new ArrayList<>();
         listaPessoas.add(pessoa1);
