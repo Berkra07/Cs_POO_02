d46161ca57fed6edb77bb3598af125f0f0121059
Notebook <bernardokravutschkegomes@gmail.com>
2026-08-18 13:08:29 -0300

    fix: reescrita das classes main para versão do Java 21

--- src/Alura/Praticas/Implementacao/vendas/application/ApkCompraNet.java
+++ src/Alura/Praticas/Implementacao/vendas/application/ApkCompraNet.java
@@ -5,7 +5,7 @@ import Alura.Praticas.Implementacao.vendas.Entities.Products;
 import java.util.Scanner;
 
 public class ApkCompraNet extends Products {
-    static void main() {
+    public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
 
         Products products = new Products();
