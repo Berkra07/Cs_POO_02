a59ee9089a8289a163637e8c21bf72e74c99367c
Notebook <bernardokravutschkegomes@gmail.com>
2026-08-24 23:31:05 -0300

    Listas

---
+++ src/Alura/Praticas/ListasEColecoesDados/Garagem/GaragemApk.java
@@ -0,0 +1,11 @@
+package Alura.Praticas.ListasEColecoesDados.Garagem;
+
+public class GaragemApk {
+    public static void main(String[] args) {
+
+        System.out.println("Seja bem vindo a Automotors");
+
+        Garagem automovel1 = new Garagem("Prisma", 19.000);
+
+    }
+}
