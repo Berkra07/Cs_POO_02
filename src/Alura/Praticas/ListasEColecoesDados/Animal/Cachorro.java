81ff606995d03d973a4767218bc4a0f13a080e5d
Win <bernardokravutschkegomes@gmail.com>
2026-08-26 20:23:07 -0300

    Studying

---
+++ src/Alura/Praticas/ListasEColecoesDados/Animal/Cachorro.java
@@ -0,0 +1,16 @@
+package Alura.Praticas.ListasEColecoesDados.Animal;
+
+public class Cachorro extends Animal{
+
+    public Cachorro(boolean docil, String sexo) {
+        super(docil, sexo);
+    }
+
+    public void sentar(){
+        System.out.println("Sentado");
+    }
+
+    public void rolar(){
+        System.out.println("Rolando");
+    }
+}
