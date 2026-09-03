ba90d3a842ed7c6ef6b1091f2bcf80cfc628ad5c
Win <bernardokravutschkegomes@gmail.com>
2026-09-02 21:28:54 -0300

    Utilizando Collection.sort

---
+++ src/Alura/Praticas/ListasEColecoesDados/ListaOrdenada02/Titulo.java
@@ -0,0 +1,11 @@
+package Alura.Praticas.ListasEColecoesDados.ListaOrdenada02;
+
+public class Titulo implements Comparable<Titulo> {
+     String nome;
+
+
+    @Override
+    public int compareTo(Titulo outroTitulo) {
+        return this.nome.compareTo(outroTitulo.nome);
+    }
+}
