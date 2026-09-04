bedc139d3fd3f6000339e76246959d88eb573a34
Win <bernardokravutschkegomes@gmail.com>
2026-09-04 12:52:39 -0300

    desafio Aplicação de Compras

---
+++ src/Alura/Desafios/AplicacaoDeCompras/CartaoCredito.java
@@ -0,0 +1,33 @@
+package Alura.Desafios.AplicacaoDeCompras;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartaoCredito {
+    private double limite;
+    private double saldo;
+    private List<Compra> compras;
+
+    public CartaoCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.compras = new ArrayList<>();
+    }
+
+    public Boolean lancaCompra ( Compra compra){
+        if (this.saldo > compra.getValor()){
+            this.saldo -= compra.getValor();
+            this.compras.add(compra);
+            return true;
+        }
+        return false;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
