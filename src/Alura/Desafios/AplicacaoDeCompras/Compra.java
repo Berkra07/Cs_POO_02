67a5771741f9a744082959f196f0d92c0e9fb813
Win <bernardokravutschkegomes@gmail.com>
2026-09-04 12:52:47 -0300

    desafio Aplicação de Compras

---
+++ src/Alura/Desafios/AplicacaoDeCompras/Compra.java
@@ -0,0 +1,31 @@
+package Alura.Desafios.AplicacaoDeCompras;
+
+public class Compra implements Comparable<Compra>{
+
+    private String descricao;
+    private double valor;
+
+    public Compra(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Compra : " + descricao + " - R$" + valor;
+    }
+
+    @Override
+    public int compareTo(Compra outraCompra) {
+        return Double.compare(this.valor,outraCompra.valor);
+    }
+}
